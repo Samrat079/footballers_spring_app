07e81fe9d37f4d10def1e0756786694f5d53cbf5
Samrat079 <samratdutta079@gmail.com>
2026-02-25 22:18:09 +0530

    Added cross origin fix

--- src/main/java/com/example/footballer_db/footballers/footballer_controller.java
+++ src/main/java/com/example/footballer_db/footballers/footballer_controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/v1/footballer")
 public class footballer_controller {
--- src/main/java/com/example/footballer_db/test/test_controller.java
+++ src/main/java/com/example/footballer_db/test/test_controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/v1/test")
 public class test_controller {
